43b9ce33a072ed7515cc818f81b35c9a9004b7ce
Nadya Chupakhina <pinnadya@gmail.com>
2023-02-07 13:50:36 +0100

    init project

--- web/public/index.php
+++ web/public/index.php
@@ -1,7 +1,9 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+use App\Kernel;
 
-echo "Welcome here";die();
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
---
+++ web/src/Entity/Comment.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommentRepository::class)]
+class Comment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $authorName = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $text = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuthorName(): ?string
+    {
+        return $this->authorName;
+    }
+
+    public function setAuthorName(string $authorName): self
+    {
+        $this->authorName = $authorName;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+}
